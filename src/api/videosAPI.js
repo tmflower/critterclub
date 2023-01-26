de359f404bd7ba104232572ed057d0ac60d5d67e
Tina Flower <tmflower@gmail.com>
2023-01-25 17:14:32 -1000

    fixed urls

--- src/api/videosAPI.js
+++ src/api/videosAPI.js
@@ -7,11 +7,11 @@ import axios from 'axios';
  */
 
 let MEDIA;
-const BASE_URL_VIDEOS = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class VideosAPI {
     static async getVideo(animalName) {
-        const res = await axios.get(`${BASE_URL_VIDEOS}/util/media`);
+        const res = await axios.get(`${BASE_URL}/util/media`);
         MEDIA = res.data.media;
         const videoId = `${MEDIA[animalName].video}`;
         return videoId;
