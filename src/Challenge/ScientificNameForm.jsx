666f19a4a6f28af9abbed50f2834e1cd061816e4
Tina Flower <94068349+tmflower@users.noreply.github.com>
2023-02-13 14:55:51 -1000

    Update ScientificNameForm.jsx

--- src/Challenge/ScientificNameForm.jsx
+++ src/Challenge/ScientificNameForm.jsx
@@ -32,7 +32,7 @@ export function ScientificNameForm({
 
     // compare user's selection to animal data to check if correct; provide corresponding feedback message
     // update user's points
-    // increment numQuestions; when numQuestions is >=3, user can submit answers and earn badge
+    // increment numQuestions; when numQuestions is >=2, user can submit answers and earn badge
     const handleSubmit = (e) => {
         // compare user's selection to animal data to check if correct; provide corresponding feedback message
         e.preventDefault();
